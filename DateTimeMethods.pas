bd53a15dbf4abc4ba72174de7ff1919dafb6dc30
unknown <rodion.bezuglov@yandex.ru>
2019-12-02 16:31:13 +0300

    LaterInYear

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -1,9 +1,9 @@
-unit DateTimeMethods;
+﻿unit DateTimeMethods;
 
 function IsLeapYear(year: integer): boolean := year mod 4 = 0;
 function DaysInMonth(month, year: integer): integer;
 begin
-case month of:
+case month of
 	1, 3, 5, 7, 8, 10, 12: result := 31;
 	2: begin
 		result := 28;
@@ -12,9 +12,25 @@ case month of:
 	end;
 	else
 		result:=30;
+  end;
 end;
-function LaterInDay(p1, p2: DateTime): DateTime := p1;
-function LaterInYear(p1, p2: DateTime): DateTime := p1;
+function LaterInYear(p1, p2: DateTime): DateTime;
+begin
+  if p1.Month <> p2.Month then
+  begin
+    if p1.Month > p2.Month then
+      result := p1
+    else
+      result := p2;
+  end
+  else
+  begin
+    if p1.day > p2.day then
+      result := p1
+    else
+      result := p2;
+  end;
+end;
 function DaysInYear(year: integer): integer := 0;
 function DaysInYearRange(year1, year2: integer): integer := 0;
 function SecondsInHours(hours: integer): integer := 0;
